d7c0f17cad644fad958e34338056f29051c63b02
Nishtha Kohli <89636008+Nishthakohli1637@users.noreply.github.com>
2023-09-22 20:28:11 +0530

    Update 33 palindrome partitioning dp.cpp

--- 33 palindrome partitioning dp.cpp
+++ 33 palindrome partitioning dp.cpp
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-int dp[100][100];
-bool check_palin(string s, int n, int i, int j){
-    if(i==j){
-        return true;
-    }
-    if(i>j){
-        return true;
-    }
+
+bool check_palin(string s, int i, int j){
+    // if(i==j){
+    //     return true;
+    // }
+    // if(i>j){
+    //     return true;
+    // }
     while(i<j){
         if(s[i]!=s[j]){
             return false;
@@ -17,25 +17,32 @@ bool check_palin(string s, int n, int i, int j){
     }
     return true;
 }
-int palin_part(string s, int n, int i, int j){
+int palin_part(string s, int n, int i, int j, vector<vector<int>>&dp){
+    
     if(i>=j){
         return 0;
     }
     if(dp[i][j]!=-1){
         return dp[i][j];
     }
-    dp[i][j]=INT_MAX;
-    for(int k=i;k<j;k++){
-        int ans1=palin_part(s, n, i, k);
-        int ans2=palin_part(s, n, k+1, j);
-        dp[i][j]=min(ans1+ans2+1, dp[i][j]);
+    if(check_palin(s, i, j)==true){
+        dp[i][j]=0;
+        return 0;
     }
-    return dp[i][j];
+    int minv=INT_MAX;
+    
+    for(int k=i;k<j;k++){
+        int ans1=palin_part(s, n, i, k, dp);
+        int ans2=palin_part(s, n, k+1, j, dp);
+       minv=min(ans1+ans2+1, minv);
+    }
+    dp[i][j]=minv;
+    return minv;
 }
 int main(){
     string s;
     cin>>s;
     int n=s.length();
-    memset(dp, -1, sizeof(dp));
-    cout<<palin_part(s, n, 1, n-1);
+    vector<vector<int>>dp(n, vector<int>(n, -1));
+    cout<<palin_part(s, n, 0, n-1, dp);
 }
