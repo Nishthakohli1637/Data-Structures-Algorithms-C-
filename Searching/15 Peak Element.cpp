fa835bc679b6a5892cafdac4a76a6788aa93e87d
Nishtha Kohli <89636008+Nishthakohli1637@users.noreply.github.com>
2024-01-17 22:58:12 +0530

    Update 15 Peak Element.cpp

--- Searching/15 Peak Element.cpp
+++ Searching/15 Peak Element.cpp
@@ -65,3 +65,34 @@ public:
         return s;
     }
 };
+
+
+
+
+int findPeakElement(vector<int> &arr) {
+    // Write your code here
+    int n=arr.size();
+    if(n==1){
+        return 0;
+    }
+    if(arr[0]>arr[1]){
+        return 0;
+    }
+    if(arr[n-1]>arr[n-2]){
+        return n-1;
+    }
+    int s=1, e=n-2;
+    while(s<=e){
+        int mid=(s+e)/2;
+        if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+            return mid;
+        }
+        else if(arr[mid]>arr[mid-1]){
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+    }
+    return -1;
+}
